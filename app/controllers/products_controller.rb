5425a32062246ffe0b40cc9c84e815577294cb8f
masonishee <mishee2010@gmail.com>
2023-08-28 18:52:28 -0500

    Route created

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,6 @@
 class ProductsController < ApplicationController
+  def all_products
+    all = Product.all
+    render json: { all: all }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "/all", controller: "products", action: "all_products"
 end
