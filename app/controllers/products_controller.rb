413974978c169642baad15a02246dccc7cf5d63b
masonishee <mishee2010@gmail.com>
2023-08-29 20:43:11 -0500

    Update and destroy methods added

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -18,4 +18,21 @@ class ProductsController < ApplicationController
     @product = Product.find_by(id: params[:id])
     render :show
   end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+    @product.update(
+      name: params[:name] || @product.name,
+      price: params[:price] || @product.price,
+      image_url: params[:image_url] || @product.image_url,
+      description: params[:description] || @product.description,
+    )
+    render :show
+  end
+
+  def destroy
+    @product = Product.find_by(id: params[:id])
+    @product.destroy
+    render json: { message: "Product successfully destroyed." }
+  end
 end
