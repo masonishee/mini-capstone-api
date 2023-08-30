5eff183c4a891f1f46d9893da7cdefad8f0cbe4e
masonishee <mishee2010@gmail.com>
2023-08-29 19:48:52 -0500

    Added params code to controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,8 +4,18 @@ class ProductsController < ApplicationController
     render :index
   end
 
+  def create
+    @product = Product.create(
+      name: params[:name],
+      price: params[:price],
+      image_url: params[:image_url],
+      description: params[:description],
+    )
+    render :show
+  end
+
   def show
-    @product = Product.find_by(id: params["id"])
+    @product = Product.find_by(id: params[:id])
     render :show
   end
 end
