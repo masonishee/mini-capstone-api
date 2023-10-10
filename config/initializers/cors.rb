33ed9d7f27690aafa28d0bd6018a54291fc1cafe
masonishee <mishee2010@gmail.com>
2023-10-09 20:52:57 -0500

    Fix cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "mini-capstone.peterxjang.com", "localhost:5173"
+    origins "mini-capstone.peterxjang.com", "localhost:5173", "https://mini-capstone-frontend2.onrender.com"
     resource "*", headers: :any, methods: [:get, :post, :patch, :put, :delete]
   end
 end
